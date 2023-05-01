f030e2089640fd30db4aaf91c18fe45d463728d3
muktocse <mdmukterhossain317@gmail.com>
2023-05-01 14:46:21 +0600

    python variable

---
+++ Python Variables/Variables.py
@@ -0,0 +1,29 @@
+#Creating Variables
+x = 10;
+y = "Mukter";
+print(x);
+print(y);
+#Casting
+x = str(10); #x will be '10'
+y = int(10); # y will be 10
+z = float(10); # z will be 10.0
+print(x);
+print(y);
+print(z);
+# Get the Type
+age = 24;
+y = "Mukto"
+print(type(age));
+print(type(y));
+# Single or Double Quotes
+x = "mukter";
+# is the same as
+y = 'mukter';
+print(x);
+print(y);
+# Case Sensitive
+a = 7;
+A = "muk";
+print(a);
+# A will not overwrite a
+print(A);
--- variable.py
+++
@@ -1,5 +0,0 @@
-a = "mukter"
-print(a)
-price = 317
-text = 10
-print("price+text")
